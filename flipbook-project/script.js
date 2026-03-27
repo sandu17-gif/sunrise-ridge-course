b5cbb832746e3addd905a59dd2d450570629672c
sandu17-gif <sandu17@gmail.com>
2026-03-27 13:31:11 -0700

    Add files via upload

---
+++ flipbook-project/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $("#flipbook").turn({
+    width: '100%',
+    height: 500,
+    autoCenter: true,
+    elevation: 50,
+    gradients: true,
+    duration: 1000
+  });
+});
